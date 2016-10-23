18c1a1c00765c856d2d8156aeee32e0dd85760bf
= <=>
2016-10-23 14:58:05 -0200

    Implementando deserializador de Pacient

--- src/main/java/br/com/uniCad/utils/deserializers/PacientDeserializer.java
+++ src/main/java/br/com/uniCad/utils/deserializers/PacientDeserializer.java
@@ -1,5 +1,24 @@
 package br.com.uniCad.utils.deserializers;
 
-public class PacientDeserializer {
+import java.util.List;
+
+import org.jooq.Record;
+import org.jooq.Result;
+
+import br.com.uniCad.beans.Pacient;
+
+public class PacientDeserializer extends AbstractDeserializer<Pacient> {
+
+	@Override
+	public List<Pacient> fromDataBaseResult(Result<Record> result) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pacient fromDataBaseRecord(Record record) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
