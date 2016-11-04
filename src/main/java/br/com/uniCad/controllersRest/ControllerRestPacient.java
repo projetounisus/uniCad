9ac2cc04141c06e0c6e2af1d1210ff256dd37283
projetounisus <projetounisus@gmail.com>
2016-11-04 04:51:59 -0200

    Corrigindo erros de veriosnamento

--- src/main/java/br/com/uniCad/controllersRest/ControllerRestPacient.java
+++ src/main/java/br/com/uniCad/controllersRest/ControllerRestPacient.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.uniCad.beans.Pacient;
+import br.com.uniCad.controllers.AbstractControllerRest;
 import br.com.uniCad.dao.AbstractDao;
 import br.com.uniCad.dao.PacientDao;
 
